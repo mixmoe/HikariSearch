adabd06db98f6305d180b2b003672f040d51c1a8
Mix <32300164+mnixry@users.noreply.github.com>
2022-02-20 08:38:03 +0800

    :recycle: Refactor request validate utils

--- functions/api/utils.ts
+++ functions/api/utils.ts
@@ -1,9 +1,7 @@
 import Schema from 'schemastery';
 import { StatusError } from 'itty-router-extras';
 
-type ValidateReturn<T> = T extends Schema<any, infer R> ? R : never;
-
-export async function validate<T extends Schema>(request: Request, schema: T) {
+export async function validate<S, T>(request: Request, schema: Schema<S, T>) {
   const obj = {} as { [key: string]: unknown };
   const { searchParams } = new URL(request.url),
     formData = await request.formData();
@@ -12,20 +10,14 @@ export async function validate<T extends Schema>(request: Request, schema: T) {
       ...searchParams.entries(),
       ...formData.entries(),
     ]) {
-      if (value instanceof File) {
+      try {
+        obj[key] = value instanceof File ? value : JSON.parse(value);
+      } catch {
         obj[key] = value;
-      } else {
-        try {
-          obj[key] = JSON.parse(value);
-        } catch {
-          obj[key] = value;
-        }
       }
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return schema(obj) as ValidateReturn<T>;
+    return schema(obj as unknown as S);
   } catch (err) {
-    const { message } = err as TypeError | SyntaxError;
-    throw new StatusError(400, message);
+    throw new StatusError(400, (err as TypeError).message);
   }
 }
