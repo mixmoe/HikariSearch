4545b5c1007ada83eed951a9804f84086731a83b
Mix <32300164+mnixry@users.noreply.github.com>
2022-02-26 20:29:39 +0800

    :recycle: Refactor frontend API request with namespace

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,27 +7,39 @@ export const axios = _axios.create({
   responseType: 'json',
 });
 
-function factory<R, S>(path: string): (schema: R) => Promise<S> {
-  return async (schema: R) => {
-    const form = new FormData();
-    for (const [key, value] of Object.entries(schema))
-      form.append(key, value instanceof File ? value : JSON.stringify(value));
-    return await axios.post<S>(path, form).then((response) => response.data);
-  };
-}
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace API {
+  function factory<R, S>(path: string): (schema: R) => Promise<S> {
+    return async (schema: R) => {
+      const form = new FormData();
+      for (const [key, value] of Object.entries(schema))
+        form.append(key, value instanceof File ? value : JSON.stringify(value));
+      return await axios.post<S>(path, form).then((response) => response.data);
+    };
+  }
 
-export const api = {
-  SauceNAO: factory<types.SauceNAORequestSchema, types.SauceNAOParseResult>(
-    './SauceNAO'
-  ),
-  IqDB: factory<types.IqDBRequestSchema, types.IqDBParseResult>('./IqDB'),
-  ascii2d: factory<types.ascii2dRequestSchema, types.ascii2dParseResult>(
-    './ascii2d'
-  ),
-  EHentai: factory<types.EHentaiRequestSchema, types.EHentaiParseResult>(
-    './E-Hentai'
-  ),
-  TraceMoe: factory<types.TraceMoeRequestSchema, types.TraceMoeParseResult>(
-    './TraceMoe'
-  ),
-};
+  export const SauceNAO = factory<
+    types.SauceNAORequestSchema,
+    types.SauceNAOParseResult
+  >('./SauceNAO');
+
+  export const IqDB = factory<
+    types.IqDBRequestSchema, //
+    types.IqDBParseResult
+  >('./IqDB');
+
+  export const ascii2d = factory<
+    types.ascii2dRequestSchema,
+    types.ascii2dParseResult
+  >('./ascii2d');
+
+  export const EHentai = factory<
+    types.EHentaiRequestSchema,
+    types.EHentaiParseResult
+  >('./E-Hentai');
+
+  export const TraceMoe = factory<
+    types.TraceMoeRequestSchema,
+    types.TraceMoeParseResult
+  >('./TraceMoe');
+}
--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,16 +1,17 @@
 import { boot } from 'quasar/wrappers';
 import type { AxiosInstance } from 'axios';
-import { axios, api } from 'src/api';
+import { axios, API } from 'src/api';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $axios: AxiosInstance;
+    $api: typeof API;
   }
 }
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
-  app.config.globalProperties.$api = api;
+  app.config.globalProperties.$api = API;
 });
 
-export { api };
+export { API as api };
