55752495409283a4812b3faf3df9103fb029200f
Mix <32300164+mnixry@users.noreply.github.com>
2022-03-12 18:29:44 +0800

    :sparkles: Support dark mode and language selection

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,39 +2,60 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <q-toolbar-title>{{ $t('Hikari Anime Image Search') }}</q-toolbar-title>
+        <div v-if="$q.screen.gt.sm">Quasar v{{ $q.version }}</div>
         <q-btn
           flat
           dense
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          icon="code"
+          href="https://github.com/mixmoe/HikariSearch"
         />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-separator spaced vertical />
+        <q-btn
+          flat
+          dense
+          round
+          :icon="$q.dark.isActive ? 'dark_mode' : 'light_mode'"
+          @click="$q.dark.toggle"
+        />
+        <q-btn flat dense round icon="language" push>
+          <q-popup-proxy>
+            <q-select
+              v-model="locale"
+              :options="availableLocales"
+              :label="$t('Choose language')"
+              style="width: 250px"
+              class="q-pa-md"
+              borderless
+              emit-value
+              map-options
+              options-dense
+            >
+              <template v-slot:prepend>
+                <q-icon name="translate" color="secondary" />
+              </template>
+            </q-select>
+          </q-popup-proxy>
+        </q-btn>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onBeforeMount } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-const leftDrawerOpen = ref(false);
+const { locale } = useI18n({ useScope: 'global' });
 
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+const availableLocales = [
+  { value: 'zh-CN', label: 'Chinese Simplified' },
+  { value: 'en-US', label: 'English (United States)' },
+];
+
+onBeforeMount(() => (locale.value = navigator.language));
 </script>
