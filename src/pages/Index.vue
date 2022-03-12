f4c4cc4b5f93bcbc9951f076f114cd3ff793f807
Mix <32300164+mnixry@users.noreply.github.com>
2022-03-12 18:51:54 +0800

    :lipstick: Improve index site tab style

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -31,7 +31,7 @@
       <div class="text-h5 q-pa-md">{{ $t('Search Options') }}</div>
       <q-separator />
       <q-card-section>
-        <q-tabs v-model="tab">
+        <q-tabs v-model="tab" class="text-primary">
           <q-tab name="sauce" label="SauceNAO"></q-tab>
           <q-tab name="iq" label="IqDB"></q-tab>
           <q-tab name="ascii2d" label="ascii2d"></q-tab>
@@ -84,7 +84,6 @@ function onImageChange() {
   if (!file.value) return;
   const reader = new FileReader();
   reader.onload = ({ target }) => {
-    console.log(target?.result);
     preview.value = target?.result as string;
   };
   reader.readAsDataURL(file.value);
